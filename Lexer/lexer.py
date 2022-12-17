6a4e7627d6e590c76ed7a9492e82cd280f5217b3
Sebastian <stcedano@gmail.com>
2022-12-17 13:42:22 -0800

    Adjusted syntax

--- Lexer/lexer.py
+++ Lexer/lexer.py
@@ -6,19 +6,23 @@ class lexer():
     def __init__(self, program):
 
         self.program_lines = []
+        self.program_variables = []
+        self.program_functions = []
 
         for count, line in enumerate(program):
             self.program_lines+=[line]
         self.lines = count + 1
 
-        self.token_parser()
+        
+
 
     def token_parser(self):
         self.tokens = []
+        self.tokens_type = []
         for self.line in self.program_lines:
 
             line_str = "".join(self.line)
-            operators = r'(\s|;|:|,|-|[(|)]|\[|\])'
+            operators = r'(\s|:|,|\+|#|-|[(|)]|\[|\])'
             self.line = re.split(operators, line_str)
             self.line = [i for i in self.line if i != '']
             self.line = [i for i in self.line if i != ' ']
@@ -26,27 +30,27 @@ class lexer():
             index = 0
             for current_token in self.line:
                 token = self.check_token(current_token, index)
-                self.tokens+=[token.text]
-                index+=1
+
+                if token != None:
+
+                    self.tokens+=[token.text]
+                    self.tokens_type+=[token.kind]
+                    index+=1
+                else:
+                    break
             
         print("Successfully parsed program!")
+        print(self.tokens)
+        return self.tokens, self.tokens_type     
 
     def check_token(self, token, token_index):
         
-        if token == "set":
-            token_obj = Token(token, TokenType.SET )
-        elif token == "SCOPE":
-            token_obj = Token(token, TokenType.SCOPE )
+        if token == "let":
+            token_obj = Token(token, TokenType.LET )
+        elif token == "def":
+            token_obj = Token(token, TokenType.DEF )
         elif token.isnumeric() == True:
             token_obj = Token(token, TokenType.NUMBER )
-        elif token == "\n":
-            token_obj = Token(token, TokenType.NEWLINE )
-        elif token == "A":
-            token_obj = Token(token, TokenType.A )
-        elif token == "B":
-            token_obj = Token(token, TokenType.B )
-        elif token == "OUTPUT":
-            token_obj = Token(token, TokenType.OUTPUT )
         elif token == "INV":
             token_obj = Token(token, TokenType.INV )
         elif token == "LOAD":
@@ -55,17 +59,14 @@ class lexer():
             token_obj = Token(token, TokenType.INC )
         elif token == "CDEC":
             token_obj = Token(token, TokenType.CDEC )
-        elif token == "START":
-            token_obj = Token(token, TokenType.START )
-        elif token == "END":
-            token_obj = Token(token, TokenType.END )
-
-        elif token == ";":
-            token_obj = Token(token, TokenType.SEMICOLON )
+        elif token == "=":
+            token_obj = Token(token, TokenType.EQUAL )
         elif token == ":":
             token_obj = Token(token, TokenType.COLON )
         elif token == "-":
-            token_obj = Token(token, TokenType.DASH )
+            token_obj = Token(token, TokenType.MINUS )
+        elif token == "+":
+            token_obj = Token(token, TokenType.PLUS )
         elif token == ",":
             token_obj = Token(token, TokenType.COMMA )
         elif token == "(":
@@ -76,23 +77,35 @@ class lexer():
             token_obj = Token(token, TokenType.OPENBRACKET )
         elif token == "]":
             token_obj = Token(token, TokenType.CLOSEBRACKET )
-
-
-
-
-        elif token == "def":
-            token_obj = Token(token, TokenType.DEF )
+        elif token == "\n":
+            token_obj = Token(token, TokenType.NEWLINE )
+        elif token == "#":
+            return None
+        
         # Checks for function definition
-        elif (isinstance(token, str) == True) and (self.line[0] == "def"):
-            token_obj = Token(token, TokenType.STRING)
+        elif (isinstance(token, str) == True) and (self.previous_token(token_index) == "let") or (token in self.program_variables):
+            token_obj = Token(token, TokenType.IDENT)
 
-        elif (isinstance(token, str) == True) and (token in self.tokens):
-            token_obj = Token(token, TokenType.STRING)
+            if token not in self.program_variables:
+                self.program_variables.append(token_obj.text)
 
+        elif (isinstance(token, str) == True) and (self.line[0] == "def") or (token in self.program_functions):
+
+            token_obj = Token(token, TokenType.IDENT)
+
+            if token not in self.program_functions:
+                self.program_functions.append(token_obj.text)
+            
         else:
             self.abort(f'Unknown token "{token}" is not valid')
-
         return token_obj
+
+    def previous_token(self, index):
+
+        try:
+            return self.line[index - 1]
+        except:
+            return None
 
     def abort(self, error_code):
         print(f'I am Error: {error_code}')
@@ -106,30 +119,32 @@ class TokenType():
     NEWLINE = 0
     NUMBER = 1
     IDENT = 2
-    STRING = 3
+
     # Keywords
-    SET = 101
+    LET = 101
     DEF = 102
-    START = 103
-    END = 104
-    INV = 105
-    INC = 106
-    LOAD = 107
-    CDEC = 108
-    ARGS = 109
-    A = 110
-    B = 111
-    OUTPUT = 112
-    SCOPE = 113
-    # Operators
-    COLON = 201  
-    SEMICOLON = 202
-    COMMA = 203
-    DASH = 204
-    OPENBRACKET = 205
-    CLOSEBRACKET = 206
-    OPENPARENTHESIS = 207
-    CLOSEPARENTHESIS = 208
+    ARGS = 103
+
+    # Operations
+    CDEC = 201
+    INC = 202
+    INV = 203
+    LOAD = 204
+    
+    # Binary Operators
+    EQUAL = 301  
+    PLUS = 302
+    MINUS = 303
+    DIVIDE = 305
+    MULTIPLY = 306
+
+    # Grammar
+    COMMA = 401
+    OPENBRACKET = 402
+    CLOSEBRACKET = 403
+    OPENPARENTHESIS = 404
+    CLOSEPARENTHESIS = 405
+    COLON = 406
 
 class Token:   
     def __init__(self, tokenText, tokenKind):
--- main.py
+++ main.py
@@ -1,6 +1,63 @@
 import argparse
 from Lexer.lexer import lexer
 
+class buzz_parser():
+
+    def __init__(self):
+        pass
+
+    def parse_statements(self, program_tokens, program_types):
+
+        statements = []
+        statements_kind = []
+        current_statement = []
+        current_statement_types = []
+        index = 0
+        for current_token in program_tokens:
+
+            #print(f'Current token: {current_token}')
+            
+            if current_token != "\n":
+                current_statement.append(current_token)
+                current_statement_types.append(program_types[index])
+            else:
+                current_statement.append(current_token)
+                current_statement_types.append(program_types[index])
+                statements_kind+=[current_statement_types]
+                statements+=[current_statement]
+                current_statement = []
+                current_statement_types = []
+            index+=1
+
+
+        return statements, statements_kind
+
+
+    def match(self, statement_types):
+
+        self.environment_variables = ["A", "B", "OUTPUT"]
+        
+        for line in statement_types:
+            index = 0
+            for current_token in line:
+
+                try:
+                    next_token = line[index + 1]
+
+                except:
+                    next_token = None
+
+
+                index+=1
+
+            
+
+            
+                
+
+            
+
+        
 def main():
         
     parser = argparse.ArgumentParser(description="Buzz! programming language parser for Woodpecker CPU")
@@ -8,7 +65,15 @@ def main():
     args = parser.parse_args()
 
     with open(args.filename) as file:
-        test = lexer(file)
+        program = lexer(file)
+
+        program_tokens, program_types = program.token_parser()
+
+        #print(f'DEBUG{program_types}')
+        test = buzz_parser()
+        statements, statement_types = test.parse_statements(program_tokens, program_types)
+        test.match(statement_types)
+
 
 if __name__=="__main__":
     main()
