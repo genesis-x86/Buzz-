f6013e6a7942ea597dd00b4c536734a5a8e2b797
Sebastian <stcedano@gmail.com>
2022-12-26 18:10:46 -0800

    Finishing touches on base interpreter

--- cpu.py
+++ cpu.py
@@ -1,32 +1,46 @@
 class CPU:
 
+    # Initializes memory and register arrays
     def __init__(self, memSize=10):
 
         self.mem = [0 for n in range(memSize)]
+        self.memSize = memSize
 
 class State(CPU):
 
-
-    def __init__(self):
-        super().__init__()
+    def __init__(self, memSize):
+        super().__init__(memSize)
 
         self.register = 0
         self.memory = self.mem[self.register]
         self.save = 0
 
     def incReg(self):
-        self.register+=1
+        if self.register >= self.memSize - 1:
+            self.register = 0
+            self.memory = self.mem[0]
+
+        else:
+            self.register+=1
+        
+            self.memory = self.mem[self.register]
 
     def cdecReg(self):
         
-        if self.save == 1:
+        if self.save == 1 and self.register > 0:
             self.register-=1
+            self.memory = self.mem[self.register]
+        elif self.save == 1 and self.register <= 0:
+            self.register = self.memSize - 1
+            self.memory = self.mem[self.register]
 
     def invMem(self):
         if self.memory == 1:
             self.memory = 0
         else:
             self.memory = 1
+        
+        self.mem[self.register] = self.memory
 
     def loadMem(self):
 
@@ -35,33 +49,30 @@ class State(CPU):
         else:
             self.save = 0
 
-
     def returnState(self):
         return self.register, self.memory, self.save
 
 print("Welcome to Buzz version 2!")
+print(f'Type "help", "credits" or "license" for more information. ')
 
-state = State()
+
+state = State(20)
 
 # Main loop
 while True:
 
-    print(f'Type "help", "credits" or "license" for more information. ')
-
+    print(state.mem)
+    print(state.returnState())
 
     operation = input("Buzz > ")
     if operation == "INC":
         state.incReg()
-        print(state.returnState())
     elif operation == "CDEC":
         state.cdecReg()
-        print(state.returnState())
     elif operation == "INV":
         state.invMem()
-        print(state.returnState())
     elif operation == "LOAD":
         state.loadMem()
-        print(state.returnState())
     elif operation == "license":
         print("Code is available on github. Licensed under GNU")
     elif operation == "credits":
@@ -70,7 +81,6 @@ while True:
         print("Made by genesis")
     elif operation == "help":
         print("The woodpecker cpu is a zero-indexed array of 2^32 bits of memory, all initialized at zero. The cpu includes a binary memory register which can be accessed at any index in the array.  The cpu allows four operations: INC to increment the index, LOAD which loads the binary memory register with 1, if the memory at the current index is 1, INV which inverts the bit in the memory and CDEC which decrements the register if and only if the bit it has loaded is 1. ")
-
     else:
         print(f'Woodpecker Error: "{operation}" not valid')
 
